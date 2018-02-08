ed16feb0b8cef5dd44169598b536da449fac74de
xxl0306 <470109166@qq.com>
2018-02-08 15:53:38 +0800

    hook

--- src/main/java/com/fuiou/busitest/BusiTest.java
+++ src/main/java/com/fuiou/busitest/BusiTest.java
@@ -13,11 +13,11 @@ public class BusiTest {
 	@SuppressWarnings({ "deprecation" })
 	public static void main(String[] args) throws Exception {
 		
-	int seqNo = 258;
+	int seqNo = 268;
 		String consumeSn = "JG171117133928435" + seqNo;
 		String refundSn = "JG171117133928435" +(seqNo+1);
 		System.out.println(AesUtil.Encrypt("1", "46a0032448e74b4e"));
-		String consumeUrl = "http://192.168.8.31:11402/mflg/flgmsrv!payDispatch"+"?productNum="+URLEncoder.encode(AesUtil.Encrypt("1", "46a0032448e74b4e"))+
+		String consumeUrl = "http://192.168.8.34:11402/mflg/flgmsrv!payDispatch"+"?productNum="+URLEncoder.encode(AesUtil.Encrypt("1", "46a0032448e74b4e"))+
 //				System.out.println("http://192.168.52.51:8083/mflg/flgmsrv!payDispatch"+"?productNum="+URLEncoder.encode(AesUtil.Encrypt("1", "46a0032448e74b4e"))+
 				"&productCd="+URLEncoder.encode(AesUtil.Encrypt("etickets", "46a0032448e74b4e"))+
 				"&loginId="+URLEncoder.encode(AesUtil.Encrypt("guzhiqi1", "46a0032448e74b4e"))+
@@ -42,7 +42,7 @@ public class BusiTest {
 		if("0000".equals(AesUtil.Decrypt(json.getString("result"),"46a0032448e74b4e"))){
 			String orderNo = AesUtil.Decrypt(json.getString("orderNo"),"46a0032448e74b4e");
 			System.out.println(orderNo);
-			String refundUrl = "http://192.168.8.31:11402/mflg/reReq!refundReq?"+"orderNo="+URLEncoder.encode(AesUtil.Encrypt(orderNo, "46a0032448e74b4e"))+
+			String refundUrl = "http://192.168.8.34:11402/mflg/reReq!refundReq?"+"orderNo="+URLEncoder.encode(AesUtil.Encrypt(orderNo, "46a0032448e74b4e"))+
 					"&firmNo=pei"+
 					"&busiCd="+URLEncoder.encode(AesUtil.Encrypt("FL01", "46a0032448e74b4e"))+
 					"&serialNumber="+URLEncoder.encode(AesUtil.Encrypt(refundSn, "46a0032448e74b4e"))+
